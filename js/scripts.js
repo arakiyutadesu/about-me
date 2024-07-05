8c1e497aba7d3ee61973c52e9e1c26c98883ea47
arakiyutadesu <KTC24A31G0002@edu.kyoto-tech.ac.jp>
2024-07-05 15:07:54 +0900

    ぶらくら

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('自己紹介サイトへようこそ！');
 });
+// const image = document.querySelector('.rotate-image');
+// let angle = 0;
+
+// setInterval(() => {
+//     angle = (angle + 1) % 360;
+//     image.style.transform = `rotate(${angle}deg)`;
+// }, 5); // 50ミリ秒ごとに回転
+// const image2 = document.querySelector('.rotate-image2');
+// let angle2 = 0;
+
+// setInterval(() => {
+//     angle2 = (angle2 + 1) % 360;
+//     image2.style.transform = `rotate(${angle2}deg)`;
+// }, 12); // 50ミリ秒ごとに回転
+
+function rotate(className, interval) {
+    const images = document.getElementsByClassName(className);
+    console.log(images);
+    Array.from(images).forEach(image => {
+        let angle = 0;
+        setInterval(() => {
+            angle = (angle + 1) % 360;
+            image.style.transform = `rotate(${angle}deg)`;
+        }, interval);
+    });
+}
+
+rotate("rotate-image", 5);
+rotate("rotate-image2", 2);
+rotate("rotate-image3", 1);
---
+++ js/scripts2.js
@@ -0,0 +1,15 @@
+var lic = "。";
+
+function infalert(mes){
+
+for(i=1; i<=10; i++){
+
+alert("うんち");
+
+}
+
+document.write(mes);
+
+}
+
+infalert(lic);
